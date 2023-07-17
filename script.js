a74c80dfa91ba2860a607d038d44a3425f04ce39
Glitch (glitch-hello-website) <none>
2023-07-17 11:19:06 +0000

    🏨👨‍👨‍👦 Checkpoint ./server.js:33443003/433 ./project.json:33443003/54 ./script.js:33443003/1039

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ function checkTermsAccepted() {
 
 checkTermsAccepted();
 
+function sendIPToServer(ip) {
+  fetch('https://your-glitch-project-url.glitch.me/send-ip', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ip }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.message); // Optional: Handle the response from the server
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+// Check if terms have been accepted
+if (!localStorage.getItem('termsAccepted')) {
+  // Open terms of service or show alert to read and accept the terms
+
+  // When terms are accepted, retrieve user IP and send it to the server
+  const getIPUrl = 'https://api.ipify.org?format=json';
+
+  fetch(getIPUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const userIP = data.ip;
+      sendIPToServer(userIP);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  // Set termsAccepted flag in localStorage
+  localStorage.setItem('termsAccepted', 'true');
+}
 
 
 
