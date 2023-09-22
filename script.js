866b47213069b899a4515e2a70e1aa4854f38228
Glitch (glitch-hello-website) <none>
2023-09-22 16:54:34 +0000

    🚘⛵️ Checkpoint ./.glitch-assets:33443003/370 ./index.html:33443003/476 ./script.js:33443003/432 ./style.css:33443003/21

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 
-const correctPassword = '*okral$15#';
-
-function checkPassword() {
-  const inputPassword = prompt('Enter the password to view the content:');
-  
-  if (inputPassword === correctPassword) {
-    showContent();
-  } else {
-    alert('Incorrect password. Please try again.');
-  }
-}
-
-function showContent() {
-  const contentDiv = document.getElementById('content');
-  contentDiv.style.display = 'block';
-}
-
-window.onload = checkPassword;
-
-
 
 function autoclicker() {
         var x = document.getElementById("autoclicker");
