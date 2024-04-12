12c5cb13a8f16cdad1cbcd7400787860b878ab65
Glitch (okral) <none>
2024-04-12 19:20:48 +0000

    📠🐑 Checkpoint ./script.js:33443003/235

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-
-function autoclicker() {
-        var x = document.getElementById("autoclicker");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "block";
-        }
-      }
  
 function spammer() {
         var x = document.getElementById("spammer");
